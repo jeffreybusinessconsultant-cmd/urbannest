2b1809ba7b3ac5acff65473fef4b24b2358a745c
Sibiyoan <sibiyoanyoans@gmail.com>
2026-02-25 13:18:11 +0530

    Configure for static deployment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { ScrollToTop } from "@/components/ScrollToTop";
 import NotFound from "@/pages/not-found";
 import { Home } from "@/pages/Home";
 import { Properties } from "@/pages/Properties";
@@ -32,6 +33,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
+        <ScrollToTop />
         <Toaster />
         <Router />
       </TooltipProvider>
